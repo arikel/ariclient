9da879ed36f608072cc52076d30a1dc7e378c5dd
arikel <arikel@arikel-desktop.(none)>
2012-02-22 13:21:07 +0100

    wxclient

--- wxMapeditor.py
+++ wxMapeditor.py
@@ -5,6 +5,7 @@
 
 import wx
 import os
+import pygame
 # warning, in wx, a frame is a window, a window is a frame.
 
 class SimpleFrame(wx.Frame):
@@ -27,6 +28,13 @@ class SimpleFrame(wx.Frame):
 		
 		self.SetMenuBar(menubar)
 		self.CreateStatusBar()
+
+		self.panel = TestPanel(self)
+
+		sizer = wx.BoxSizer( wx.HORIZONTAL )
+		sizer.Add( self.panel, 0, wx.ALL, 5 )
+		self.SetSizer( sizer )
+		
 		
 		#Binds the two items to the correct funcion, AboutBox and Quit application
 		self.Bind(wx.EVT_MENU, self.OnOpen, OpenMn)
@@ -53,10 +61,36 @@ class SimpleFrame(wx.Frame):
 		about.Name = "ariclient map editor"
 		wx.AboutBox(about)
 
+		
+class TestPanel(wx.Panel):
+	def __init__(self, parent):
+		wx.Panel.__init__(self, parent, -1, size = wx.Size(640,480))
+		self.screen = pygame.Surface((640,480))
+		white = pygame.Color(255, 120, 140, 255)
+		rect = pygame.Rect(10, 10, 100, 100)
+		pygame.draw.rect(self.screen, white, rect)
+
+		self.Bind(wx.EVT_PAINT, self.OnPaint)
+
+
+	def OnPaint(self, evt):
+		#skips exceptions (like initialized before the main App)
+		try:
+			s = pygame.image.tostring(self.screen, 'RGB')  # Convert the surface to an RGB string
+			img = wx.ImageFromData(640, 480, s)  # Load this string into a wx image
+			#dc = wx.PaintDC(self)
+			bmp = wx.BitmapFromImage(img)  # Get the image in bitmap form
+			dc = wx.PaintDC(self)  # Device context for drawing the bitmap
+			dc.DrawBitmap(bmp, 0, 0, False)  # Blit the bitmap image to the display
+			del dc
+		except:
+			pass
+
+
+
 
 if __name__ == "__main__":
 	app = wx.App()
 	frame = SimpleFrame()
 	frame.Show()
 	app.MainLoop()
-	
